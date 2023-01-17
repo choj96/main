3f21316f166129db73635f236016894893161c10
choj96 <choj9603@gmail.com>
2023-01-17 18:24:16 +0900

    dddd

---
+++ src/pages/order/ModalBase.js
@@ -0,0 +1,18 @@
+// import "./Modal.css";
+
+// function ModalBasic({ setModalOpen, id, title, content, writer }) {
+//   // 모달 끄기
+//   const closeModal = () => {
+//     setModalOpen(false);
+//   };
+
+//   return (
+//     <div style={{ container }}>
+//       <button style={{ close }} onClick={closeModal}>
+//         X
+//       </button>
+//       <p>모달창입니다.</p>
+//     </div>
+//   );
+// }
+// export default ModalBasic;
